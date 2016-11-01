e939e96cffd17dc0bb142b457234d3a64546e5c2
长宏 <3200601@qq.com>
2016-11-01 15:46:54 +0800

    hello

--- src/main/java/j/ch/DB.java
+++ src/main/java/j/ch/DB.java
@@ -1,7 +1,65 @@
 package j.ch;
 
+import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 /**
  * Created by jiang on 2016/10/30 0030.
  */
-public class DB {
+public class DB implements Logmy{
+    private static DB instanse;
+
+    public static DB getInstance() {
+        if (instanse == null) {
+            instanse = new DB();
+        }
+        return instanse;
+    }
+    String url = "jdbc:mysql://localhost:3306/mysql?useSsl=false";
+    String user = "root";
+    String pass = "0000";
+
+    Connection connection;
+    private DB() {
+        try {
+//            File file = new File("info.txt");
+//                Scanner s = new Scanner(new FileInputStream(file));
+//                pass = s.nextLine().trim();
+//                s.close();
+
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void exesql(String sql) {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (statement != null) {
+
+                statement.execute(sql);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new DB().exesql("show databases  #fromjdbc  #fromjdbc || 1477895804");
+    }
 }
--- src/main/java/j/ch/Main.java
+++ src/main/java/j/ch/Main.java
@@ -2,50 +2,57 @@ package j.ch;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.core.*;
+import com.hazelcast.map.listener.EntryAddedListener;
+import com.hazelcast.map.listener.MapListener;
+
+import javax.swing.*;
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Created by jiang on 2016/10/30 0030.
  */
-public class Main implements Logmy,ItemListener<String>{
+public class Main implements Logmy,EntryAddedListener<String,String>{
     public static HazelcastInstance hazelcast = null;
-    public static IList<String> list = null;
+    public static IMap<String,String> list = null;
     static {
+//        File file = new File("info.txt");
+//        if (!file.exists()) {
+//            JOptionPane.showMessageDialog(null, "建立info.txt文件。设置root的密码");
+//            System.exit(0);
+//        }
         Config config = new Config();
         hazelcast = Hazelcast.newHazelcastInstance(config);
-        list = hazelcast.getList("list");
+        list = hazelcast.getMap("map");
     }
 
     public Main() {
-        list.addItemListener(this, true);
+        list.addEntryListener(this, true);
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Main me = new Main();
-        list.add("hello");
-        new File().upsql();
 
-
+         new Main();
+        JOptionPane.showMessageDialog(null, "启动完成");
     }
 
-    /**
-     * Invoked when an item is added.
-     *
-     * @param item the added item
-     */
     @Override
-    public void itemAdded(ItemEvent<String> item) {
-        log("itemadd " + item.getItem() + "from:" + item.getMember().toString());
-        log(String.valueOf(item.getMember() == hazelcast.getCluster().getLocalMember()));
-    }
+    public void entryAdded(EntryEvent<String, String> event) {
 
-    /**
-     * Invoked when an item is removed.
-     *
-     * @param item the removed item.
-     */
-    @Override
-    public void itemRemoved(ItemEvent<String> item) {
-        log("itemadd " + item.getItem() + "from:" + item.getMember().toString());
+            String ip= event.getMember().getAddress().toString();
+      String local= hazelcast.getCluster().getLocalMember().getAddress().toString();
+        if (ip.equals(local)) {
+            return;
+        }
+        String sql = event.getValue();
+        if (sql.contains("fromjdbc")) {
+            log("sql jdbc exit");
+            return;
+        }
+        DB.getInstance().exesql(sql+"  # fromjdbc \n");
+        log(ip+sql);
+
     }
 }
 
